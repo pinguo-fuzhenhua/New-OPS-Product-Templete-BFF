42b767f0dfd030c65f43182ed1eaad60010d7154
guzhongzhi <guzhongzhi@camera360.com>
2022-03-06 20:09:48 +0800

    debug

--- internal/infrastructure/discovery/custom_conn.go
+++ internal/infrastructure/discovery/custom_conn.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/url"
 	"time"
 
 	"github.com/go-kratos/kratos/v2/registry"
 	kgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 )
 
 type CustomGRPCConn interface {
@@ -18,10 +20,10 @@ type CustomGRPCConn interface {
 }
 
 type CustomConn struct {
-	conn        CustomGRPCConn
 	conns       []CustomGRPCConn
 	serviceName string
 	factory     func(opts ...kgrpc.ClientOption) (*grpc.ClientConn, error)
+	endpoints   []string
 }
 
 func NewCustomConn() *CustomConn {
@@ -46,14 +48,21 @@ func (s *CustomConn) close() error {
 func (s *CustomConn) Notify(instances []*registry.ServiceInstance) {
 	go func() {
 		time.Sleep(time.Second * 5)
+		newEndpoints := []string{}
 		for _, ins := range instances {
 			for _, endpoint := range ins.Endpoints {
 				fmt.Println(endpoint)
 				tmp, _ := url.Parse(endpoint)
 				fmt.Println(tmp.Host)
-				s.Connect(kgrpc.WithEndpoint(tmp.Host))
+				err := s.Connect(kgrpc.WithEndpoint(tmp.Host))
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					newEndpoints = append(newEndpoints, endpoint)
+				}
 			}
 		}
+		s.endpoints = newEndpoints
 	}()
 }
 
@@ -65,27 +74,29 @@ func (s *CustomConn) Connect(opts ...kgrpc.ClientOption) error {
 		return err
 	}
 	for i := 0; i < 5; i++ {
-		time.Sleep(time.Second)
+		if conn.GetState() == connectivity.Ready {
+			break
+		}
 		fmt.Println(conn.GetState().String())
+		time.Sleep(time.Second)
 	}
 	s.conns = append(s.conns, conn)
-	if len(s.conns) > 1 && false {
-		max := len(s.conns) - 1
-		for i := 0; i < max; i++ {
-			fmt.Println("close old connection")
-			go func(cn CustomGRPCConn) {
-				time.Sleep(time.Second * 60)
-				cn.Close()
-			}(s.conns[i])
-		}
-		s.conns = s.conns[max:]
-	}
 	return nil
 }
 
 func (s *CustomConn) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, opts ...grpc.CallOption) error {
-	fmt.Println(s.conns[len(s.conns)-1].(*grpc.ClientConn).GetState())
-	return s.conns[len(s.conns)-1].Invoke(ctx, method, args, reply, opts...)
+	maxIdx := len(s.conns) - 1
+	fmt.Println(s.conns[maxIdx].(*grpc.ClientConn).GetState())
+	conn := s.conns[maxIdx]
+	if ac := len(s.endpoints); ac > 1 {
+		i := int(rand.Float64()*100) % ac
+		idx := maxIdx - ac + 1 + i
+		if idx > maxIdx {
+			idx = maxIdx
+		}
+		conn = s.conns[idx]
+	}
+	return conn.Invoke(ctx, method, args, reply, opts...)
 }
 
 func (s CustomConn) Close() error {
@@ -94,5 +105,5 @@ func (s CustomConn) Close() error {
 
 // NewStream begins a streaming RPC.
 func (s *CustomConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-	return s.conn.NewStream(ctx, desc, method, opts...)
+	return s.conns[0].NewStream(ctx, desc, method, opts...)
 }
