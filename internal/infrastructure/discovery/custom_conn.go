de7be6660d5bfe56d8ca51efbcd3b9f6181ed201
guzhongzhi <guzhongzhi@camera360.com>
2022-03-14 16:47:40 +0800

    adjust deployment

--- internal/infrastructure/discovery/custom_conn.go
+++ internal/infrastructure/discovery/custom_conn.go
@@ -102,7 +102,6 @@ func (s *CustomConn) pickup() grpc.ClientConnInterface {
 	if s.endpointCount > 1 {
 		x := atomic.AddInt64(&s.count, 1)
 		idx := int(x)%s.endpointCount + s.offset
-		s.logger.Debugf("index=%v, endpointcount=%v,totalLength=%v", idx, s.endpointCount, len(s.conns))
 		conn = s.conns[idx]
 	}
 	return conn
